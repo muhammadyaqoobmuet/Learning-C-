34649f3ce204349128996fd76410c77f7f76442e
muhammadyaqoobmuet <121377745+muhammadyaqoobmuet@users.noreply.github.com>
2022-12-26 16:56:34 +0500

    Add files via upload

---
+++ scope of varrable/scope_varriable.cpp
@@ -0,0 +1,12 @@
+/* The scope of a variable refers to the region of the program in
+which the variable is accessible
+
+# there are two types of scope of varriable
+
+1- Local Varriable:A variable with local scope  that  declared outside of any function or block of code 
+accessible within the  block of code  in which it is defined
+
+2- Golobal Varriable : A global variable is a variable 
+that is declared outside of any function or block of code and accessible anywhere in program
+
+*/
