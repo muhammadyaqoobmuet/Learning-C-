519fb1a2eb15e0ca00351b349b50a657d41216a2
muhammadyaqoobmuet <121377745+muhammadyaqoobmuet@users.noreply.github.com>
2022-12-26 16:56:03 +0500

    What is varriable and coment

---
+++ varriable and coment/variable_coment.cpp
@@ -0,0 +1,42 @@
+/*varriables are name given to objects and varriables store that objects so var are cont
+ what is coment - you know that */
+
+
+
+
+/*  In C++, a data type is a classification of data 
+ that determines the type of value that a variable can hold
+ three data types 
+ 1- built in
+ 2- user-defined
+ 3-Derived 
+-----------------------------------------------------------------------------------
+ built-in data types
+int x = 10;
+float y = 3.14;
+double z = 1.234e5; long after decimal or eg 3.1415.....
+char ch = 'A';
+bool flag = true;
+*/
+
+
+
+/* how to write multi line coment-given below
+
+ this is multi line 
+comnt yes 
+this is */
+
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int a = 6,b=3;
+
+    cout<<"printing the sum of a and b "<< a+b<<endl;
+    return 0;
+}
+
+
