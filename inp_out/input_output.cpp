6a699dadae461759cd9332a1dc67dd2753016008
muhammadyaqoobmuet <121377745+muhammadyaqoobmuet@users.noreply.github.com>
2022-12-26 16:56:54 +0500

    Add files via upload

---
+++ inp_out/input_output.cpp
@@ -0,0 +1,12 @@
+//c++ come with iostream library for output and input
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter The FIrst Number: ";
+    cin>>num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;  // '>>' is extraction operator
+    cout<<"The Sum of two number is "<<num1+num2<<endl;
+return 0;
+}
